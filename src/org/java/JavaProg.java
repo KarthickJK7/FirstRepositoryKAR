ce654a2850264837a7402da479312b44543de9a0
KarthickJK7 <j.karthick.mca@gmail.com>
2020-02-03 11:15:40 +0530

    New

---
+++ src/org/java/JavaProg.java
@@ -0,0 +1,33 @@
+package org.java;
+
+public class JavaProg {
+	public static void main(String[] args) {
+		
+		//System.out.println("java progm");
+		
+		double a = 2;
+		double b = 0.1;
+		
+		if (0.1 > a) {
+			
+			System.out.println("java progm");
+			
+		}
+		
+		while(b < a)
+		{
+			
+			System.out.println("HIII");
+			b++;
+			
+			if (b == a) {
+				
+				System.out.println("Closed");
+				break;
+				
+			}
+		}
+		
+	}
+
+}
